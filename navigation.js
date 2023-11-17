bf72ac964816bfc89d0d9f622ad16315a113ca8a
Abdul <textabdulhere@gmail.com>
2023-11-17 08:27:18 +0530

    Added Dropdown Menuu in primary navigation

--- navigation.js
+++ navigation.js
@@ -1,16 +1,18 @@
 // Selectors
 
 const HEADER = document.querySelector("#header");
+const PRIMARY_DROPDOWN_BTN = document.querySelector("#primary-dropDownBtn");
+const HAMBURGER_BTN = document.querySelector("#hamburgerBtn");
 const PRIMARY_NAVIGATION = document.querySelector(".primary_navigation");
 const LOGIN_AND_SIGNUP = document.querySelector(".logIn-and-SignUp");
 
-// Event
-HEADER.addEventListener("click", (event) => {
-  if (
-    event.target.className === "dropDownBtn" ||
-    event.target.className === "hamburgerBtn"
-  ) {
-    PRIMARY_NAVIGATION.classList.toggle("view");
-    LOGIN_AND_SIGNUP.classList.toggle("view");
-  }
+// Events
+PRIMARY_DROPDOWN_BTN.addEventListener("click", () => {
+  PRIMARY_NAVIGATION.classList.toggle("view");
+  LOGIN_AND_SIGNUP.classList.toggle("view");
+});
+
+HAMBURGER_BTN.addEventListener("click", () => {
+  PRIMARY_NAVIGATION.classList.toggle("view");
+  LOGIN_AND_SIGNUP.classList.toggle("view");
 });
